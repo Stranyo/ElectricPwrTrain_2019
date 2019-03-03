085c4edba2cbfbab4a55d28702816442393ef46d
Stranyoczky Laszlo <stranyo04@gmail.com>
2019-03-03 15:46:08 +0100

    Equiations

--- Matlab/Asyncronous_motor/Asyncronous_motor.m
+++ Matlab/Asyncronous_motor/Asyncronous_motor.m
@@ -1,4 +1,4 @@
-v=400; %voltage in [v]
+U1=400; %voltage in [v]
 Xl1=5; %primer reactance in Ohm
 Xl2=6; %secunder reactance in Ohm
 R1=20; %primer resistance in Ohm
@@ -6,7 +6,7 @@ R2=21; %secunder resistance in Ohm
 n_n= 2982; % nominal rotation speed [RPM]
 p=2; %number of poles
 f=50; %input voltage frequency [Hz]
-n_s=(60*f)/p; %synchronous rotation speed [RPM]
+n_s=(60*f)/(p/2); %synchronous rotation speed [RPM]
 s=((n_s)-(n_n))/(n_s); %slip in [%]
 Rs=(1-s)/s; %slip dependent resistance in Ohm
 I_n= 197; %nominal current [A]
